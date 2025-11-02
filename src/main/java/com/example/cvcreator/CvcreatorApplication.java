9734329c0243f0e6a37493b68ed5b4332f7c91bf
Konrad <konjasinski6@gmail.com>
2025-11-02 14:06:44 +0100

    first commit

---
+++ src/main/java/com/example/cvcreator/CvcreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.cvcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvcreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvcreatorApplication.class, args);
+    }
+
+}
